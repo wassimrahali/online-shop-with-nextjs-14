ea910527f18b724f8d2fa96cd8b44477508fa74c
Wassim Rahali <wassimrahali40@gmail.com>
2024-08-24 19:45:24 +0100

    make add to cart option

--- src/Componnents/AddToCartButton/AddToCartButton.tsx
+++ src/Componnents/AddToCartButton/AddToCartButton.tsx
@@ -3,13 +3,16 @@ import icon from "../../app/assets/shopping-cart-icon.svg";
 import Image from "next/image";
 
 import { useCart } from "@/Context/CartContext";
+import Link from "next/link";
 const AddToCartButton = () => {
   const { addToCart } = useCart();
 
   return (
-    <div className="flex justify-between items-center space-x-2 cursor-pointer">
-      <Image src={icon} width={25} height={25} alt="icon-cart" />
-      <p>({addToCart})</p>
+    <div className="flex justify-between items-center space-x-2">
+      <Link className="flex justify-between items-center space-x-2 cursor-pointe" href="/order">
+        <Image src={icon} width={25} height={25} alt="icon-cart" />
+        <p>({addToCart})</p>
+      </Link>
     </div>
   );
 };
